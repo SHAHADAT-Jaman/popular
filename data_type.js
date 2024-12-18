67d4108b6962f84e2fdae4522a57801ee28adc74
Shahadat <shahadatjaman4975@gmail.com>
2024-12-18 11:07:40 +0600

    Add: data_type Js

---
+++ data_type.js
@@ -0,0 +1,25 @@
+let country = "Bangladesh";
+let capital = "Dhaka";
+let user = "shahadat";
+let sentence = `Hello, 
+This is ${user}, speaking from ${capital}, ${country}. 
+Thanks`;
+
+console. log(typeof sentence);
+
+let height = 300;
+console. log(typeof height);
+
+
+let iscold = true;
+console. log(typeof iscold);
+
+let dogName = "Jack" ;
+console. log(dogName);
+
+let wallet = null;
+console. log(wallet);
+
+let count = 45;
+console. log(45-"tes");
+
